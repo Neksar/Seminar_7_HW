8ec15caa1fc610bd52679753ed6e0a258cc59c77
Maksim Bondarenko <m.bondarenko1988@yandex.ru>
2023-02-09 20:34:52 +0300

    Done second homework to seven seminar about C sharp

---
+++ homework_2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int InputRowsColums(string str)
+{
+    int number;
+    string? text;
+    while (true)
+    {
+        System.Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Вы ввели не число");
+    }
+    return number;
+}
+
+int[,] FillRandomArray(int rows, int colums)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            matrix[i, j] = rand.Next(0, 100);
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+    return matrix;
+}
+
+void FindElementInArray(int[,] matrix)
+{
+    int row = InputRowsColums("Введите элемент строки: ");
+    int colum = InputRowsColums("Введите элемент столбца: ");
+    if (row < matrix.GetLength(0) && colum < matrix.GetLength(1))
+    {
+        System.Console.WriteLine($"Искомый элемент - {matrix[row, colum]}");
+    }
+    else
+    {
+        System.Console.WriteLine("Такого элемента в массиве нет");
+    }
+}
+
+int rows = InputRowsColums("Введите колличество строк массива: ");
+int colums = InputRowsColums("Введите колличество столбцов массива: ");
+int[,] num = FillRandomArray(rows, colums);
+FindElementInArray(num);
